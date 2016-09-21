b2695825f5c081695cc48653d08b06a395d1105e
mataviejitas <drake5000j@hotmail.com>
2016-09-21 10:57:08 -0500

    Update factorial.py

--- python/factorial.py
+++ python/factorial.py
@@ -3,5 +3,46 @@ def factorial (numero):
   if numero == 0:
     return 1
   else:  
-    return numero + factorial (numero - 2)
+    return numero + factorial (n
 factorial(12)
+dfsfjdsgf
+gjdkgsd
+gfsjdklgjskldg
+sjdklñgjdglsd
+asdfgad
+fghad
+fgad
+fhadf
+hadfh
+adhjasfgjafgj
+agjha
+gjaf
+gjafgj
+afg
+nhzcv
+h
+sfgjs
+    
+    d
+    vgfsd
+    gsdg
+    bdg
+      s
+      fg
+      adfhsrthjsrt
+      hj    d
+      fgh
+      adfh
+      adfh
+      adf
+          dsghdfsgh
+          hdasadf
+          hadfha
+          dfh d
+                
+                dfh
+                dhasdfh
+                afh
+                dfhdshdfh
+                dhsdfh
+                    fghfghfghdfgh
